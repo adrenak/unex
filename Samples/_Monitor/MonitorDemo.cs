019090f453cd43752eb73724dbe59ca360f83f74
Vatsal Ambastha <adrenak_@outlook.com>
2020-03-12 21:32:10 +0530

    feat: Improved Runner.cs
    Runner now exposes some features as static methods. GameObject hosts are
    not required any more. Reorganization of the project directory structure
    to separate the samples.
    
    BREAKING CHANGE: Runner class has breaking changes

---
+++ Samples/_Monitor/MonitorDemo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Adrenak.Unex;
+
+namespace Adrenak.Unex.Samples{
+    public class MonitorDemo : MonoBehaviour {
+        public GameObject toMonitor;
+	
+	    void Start () {
+            toMonitor.GetMonitor().HandleCollisionEnter(collision => {
+                Debug.Log("Object Collided with " + collision.collider.name);
+            });
+	    }
+    }
+}

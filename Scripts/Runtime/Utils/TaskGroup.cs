e0befb41e034f1b13548575483de29af5769b2ff
Vatsal Ambastha <adrenak_@outlook.com>
2020-08-10 18:49:53 +0530

    feat: Add TaskGroup

---
+++ Scripts/Runtime/Utils/TaskGroup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Adrenak.Unex {
+    public class TaskGroup<T, K> {
+        public event Action<K, int> SingleTaskCompleted;
+
+        Dictionary<T, Task<K>> taskMap;
+
+        public TaskGroup(Dictionary<T, Task<K>> map) {
+            taskMap = map;
+        }
+
+        public async Task<Dictionary<T, K>> Run() {
+            var resultMap = new Dictionary<T, K>();
+
+            while (taskMap.Count != 0) {
+                var done = await Task.WhenAny(taskMap.Values.ToList());
+                var index = taskMap.Values.ToList().IndexOf(done);
+                var genre = taskMap.Keys.ToList()[index];
+
+                resultMap.Add(genre, done.Result);
+                SingleTaskCompleted?.Invoke(done.Result, index);
+                taskMap.Remove(genre);
+            }
+
+            return resultMap;
+        }
+    }
+}

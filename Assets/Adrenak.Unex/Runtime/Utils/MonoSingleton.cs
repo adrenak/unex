7d9d71bcc6b74112f8eb386d74303fa9ef54e16e
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2020-11-21 15:32:44 +0530

    feat: Remove Runner and make some minor changes to code

--- Assets/Adrenak.Unex/Runtime/Utils/MonoSingleton.cs
+++ Assets/Adrenak.Unex/Runtime/Utils/MonoSingleton.cs
@@ -2,21 +2,21 @@
 
 namespace Adrenak.Unex {
     public class MonoSingleton<T> : MonoBehaviour where T : Component {
-        private static T _instance;
+        private static T instance;
         public static T Instance {
             get {
-                if (_instance == null) {
+                if (instance == null) {
                     var objs = FindObjectsOfType(typeof(T)) as T[];
                     if (objs.Length > 0)
-                        _instance = objs[0];
+                        instance = objs[0];
                     if (objs.Length > 1) 
                         Debug.LogWarning("There is more than one " + typeof(T).Name + " in the scene.");
-                    if (_instance == null) {
+                    if (instance == null) {
                         GameObject obj = new GameObject();
-                        _instance = obj.AddComponent<T>();
+                        instance = obj.AddComponent<T>();
                     }
                 }
-                return _instance;
+                return instance;
             }
         }
     }
